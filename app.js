7e107a92f210cef1405b3b6749bae4dbe51b46fc
Gautam Sidhwani <gautamsidhwani405@gmail.com>
2024-07-05 02:27:45 +0530

    JS gone

--- app.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('button');
-button.addEventListener('click',()=>{
-const h2 = document.createElement('h2');
-	h2.textContent = "This is me hello!";
-	const body = document.body;
-	body.append(h2);
-})
